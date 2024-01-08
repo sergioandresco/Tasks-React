e8cd3376706ba2dfbc2a1ea7075d7e04841dd307
sergioandresco <programador2@interactiva.net.co>
2024-01-08 07:03:15 -0500

    creation validate completed function in the taskCounter for the validate complete all tasks

--- src/components/TaskCounter.jsx
+++ src/components/TaskCounter.jsx
@@ -4,6 +4,13 @@ function TaskCounter({ total, completed }){
 
     return(
 
+        //este operador ternario es para decir si todos los task estan completados, saque el mensaje de completado, si no, siga con el conteo de los tasks
+        completed === total ?
+        
+        <h1 className="message_task"> <strong>Congratulations, you are completed the all tasks</strong></h1>
+
+        :
+
         <h1 className="message_task">
             You are completed <span>{ completed }</span> of <span>{ total }</span> Tasks
         </h1>
